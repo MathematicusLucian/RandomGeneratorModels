f2ee152f33b707fbdfefeceda4830e93b5fa4149
k1n4kut4 <recruitlukejones@gmail.com>
2024-02-21 16:06:01 +0000

    Cloned model 'three' as 'four' - to try Numpy and ArgMax

--- python_model/four/run.py
+++ python_model/four/run.py
@@ -1,16 +1,14 @@
-from randomgen import RandomGen
+from argmaxnumpy import RandomGenNumpyArgMax
 
 if __name__ == '__main__':
     # Example usage and test cases
-    random_gen = RandomGen()
-    random_gen._random_nums = [-1, 0, 1, 2, 3]
-    random_gen._probabilities = [0.01, 0.3, 0.58, 0.1, 0.01]
-    random_gen._results = {}
+    random_nums = [-1, 0, 1, 2, 3]
+    probabilities = [0.01, 0.3, 0.58, 0.1, 0.01]
+    random_gen = RandomGenNumpyArgMax(random_nums, probabilities)
     num_iterations = 100
     results = {}
     for _ in range(num_iterations):
         results = random_gen.next_num()
-        print(1)
     # Print results
     for num, count in results.items():
         print(f"{num}: {count} times")
