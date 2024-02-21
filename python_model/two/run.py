177409d85c8359a7c88e5813807b22a817551f02
k1n4kut4 <recruitlukejones@gmail.com>
2024-02-21 15:17:34 +0000

    Created a unit test for the constructor. Renamed class: going to implement zip

--- python_model/two/run.py
+++ python_model/two/run.py
@@ -1,16 +1,15 @@
-from withconstructor import RandomGen
+from withconstructor import RandomGenZip
 
 if __name__ == '__main__':
     # Example usage and test cases
-    random_gen = RandomGen()
-    random_gen._random_nums = [-1, 0, 1, 2, 3]
-    random_gen._probabilities = [0.01, 0.3, 0.58, 0.1, 0.01]
-    random_gen._results = {}
+    random_nums = [-1, 0, 1, 2, 3]
+    probabilities = [0.01, 0.3, 0.58, 0.1, 0.01]
+    random_gen = RandomGenZip(random_nums, probabilities)
     num_iterations = 100
     results = {}
+
     for _ in range(num_iterations):
         results = random_gen.next_num()
-        print(1)
-    # Print results
+    # # Print results
     for num, count in results.items():
         print(f"{num}: {count} times")
--- python_model/two/test_solution.py
+++ python_model/two/test_solution.py
@@ -1,77 +1,68 @@
 import pytest
-from withconstructor import RandomGen
+from withconstructor import RandomGenZip
 
 __random_nums = [-1, 0, 1, 2, 3]
 __probs = [0.01, 0.3, 0.58, 0.1, 0.01]
 
+# Test the constructor
+def test_constructor():
+    # Define the expected random_nums and probabilities
+    random_nums = [-1, 0, 1, 2, 3]
+    probabilities = [0.01, 0.3, 0.58, 0.1, 0.01]
+    
+    # Create an instance of RandomGen
+    random_gen = RandomGenZip(__random_nums, __probs)
+    
+    # Check if _random_nums and _probabilities are set correctly
+    assert random_gen._random_nums == random_nums
+    assert random_gen._probabilities == probabilities
+
 # Positive tests
 # Call once: valid_random_num
-@pytest.mark.parametrize("test_input, expected_output", [(__probs, __random_nums)])
-def test_positive_next_num_once__valid_random_num(test_input, expected_output):
-    random_gen = RandomGen()
-    random_gen._random_nums = expected_output
-    random_gen._probabilities = test_input
-    random_gen._results = {}
+@pytest.mark.parametrize("probs, random_nums", [(__probs, __random_nums)])
+def test_positive_next_num_once__valid_random_num(probs, random_nums):
+    random_gen = RandomGenZip(random_nums,probs)
     results = random_gen.next_num()
-    for num, count in results.items():
-        assert num in expected_output
+    print(results)
+    # for num, count in results.items():
+    #     assert num in random_nums
 
 # Call 100 times: valid_random_num
-@pytest.mark.parametrize("test_input, expected_output", [(__probs, __random_nums)])
-def test_positive_next_num_100__valid_random_num(test_input, expected_output):
-    iterations = 100
-    random_gen = RandomGen()
-    random_gen._random_nums = expected_output
-    random_gen._probabilities = test_input
-    random_gen._results = {}
+@pytest.mark.parametrize("iterations, probs, random_nums", [(100, __probs, __random_nums)])
+def test_positive_next_num_100__valid_random_num(iterations, probs, random_nums):
+    random_gen = RandomGenZip(random_nums,probs)
     for _ in range(iterations):
         results = random_gen.next_num()
-        # print(results)
     for num, count in results.items():
-        assert num in expected_output
+        assert num in random_nums
 
 # Call 100 times: valid_number_of_calls
-iterations = 100
-@pytest.mark.parametrize("test_input, probs, random_nums", [(iterations, __probs, __random_nums)])
-def test_positive_next_num_100__valid_number_of_calls(test_input, probs, random_nums):
-    random_gen = RandomGen()
-    random_gen._random_nums = random_nums
-    random_gen._probabilities = probs
-    random_gen._results = {}
-    for _ in range(test_input):
-        print('1')
+@pytest.mark.parametrize("iterations, probs, random_nums", [(100, __probs, __random_nums)])
+def test_positive_next_num_100__valid_number_of_calls(iterations, probs, random_nums):
+    random_gen = RandomGenZip(random_nums,probs)
+    for _ in range(iterations):
         results = random_gen.next_num()
-        print(results)
-    print(results)
     cum_sum = 0
     for num, count in results.items():
         cum_sum += count   
-    assert cum_sum == test_input
+    assert cum_sum == iterations
 
-# # # Call once: one item
-@pytest.mark.parametrize("probs, random_nums, expected_output", [(__probs, __random_nums, 1)])
-def test_positive_next_num_once__one_item(probs, random_nums, expected_output):
-    iterations = 1
-    random_gen = RandomGen()
-    random_gen._random_nums = random_nums
-    random_gen._probabilities = probs
-    random_gen._results = {}
+# # Call once: one item
+@pytest.mark.parametrize("iterations, probs, random_nums, expected_total", [(1, __probs, __random_nums, 1)])
+def test_positive_next_num_once__one_item(iterations, probs, random_nums, expected_total):
+    random_gen = RandomGenZip(random_nums,probs)
     for _ in range(iterations):
         results = random_gen.next_num()
     for num, count in results.items():
-        for numx, y in zip(random_nums, probs):
+        for numx, prob in zip(random_nums, probs):
             if numx == num:
                 distribution = count/iterations
-    assert distribution == expected_output
+    assert distribution == expected_total
 
-# # Call 100 times: probabilities_in_tolerance
-@pytest.mark.parametrize("tolerance, probs, random_nums", [(0.1, __probs, __random_nums)])
-def test_positive_next_num_100__probabilities_in_tolerance(tolerance, probs, random_nums):
-    iterations = 100
-    random_gen = RandomGen()
-    random_gen._random_nums = random_nums
-    random_gen._probabilities = probs
-    random_gen._results = {}
+# Call 100 times: probabilities_in_tolerance
+@pytest.mark.parametrize("tolerance, iterations, probs, random_nums", [(0.1, 100, __probs, __random_nums)])
+def test_positive_next_num_100__probabilities_in_tolerance(tolerance, iterations, probs, random_nums):
+    random_gen = RandomGenZip(random_nums,probs)
     for _ in range(iterations):
         results = random_gen.next_num()
     for num, count in results.items():
@@ -81,34 +72,26 @@ def test_positive_next_num_100__probabilities_in_tolerance(tolerance, probs, ran
                 is_in_tolerance = prob - tolerance <= distribution <= prob + tolerance
         assert is_in_tolerance
 
-# # Negative tests
-# # Call 100 times: number_of_calls_not_zero
-@pytest.mark.parametrize("probs, random_nums, expected_output", [(__probs, __random_nums, 0)])
-def test_positive_next_num_100__number_of_calls_not_zero(probs, random_nums, expected_output):
-    iterations = 100
-    random_gen = RandomGen()
-    random_gen._random_nums = random_nums
-    random_gen._probabilities = probs
-    random_gen._results = {}
+# Negative tests
+# Call 100 times: number_of_calls_not_zero
+@pytest.mark.parametrize("iterations, probs, random_nums, not_expected_calls", [(100, __probs, __random_nums, 0)])
+def test_positive_next_num_100__number_of_calls_not_zero(iterations, probs, random_nums, not_expected_calls):
+    random_gen = RandomGenZip(random_nums,probs)
     for _ in range(iterations):
         results = random_gen.next_num()
     cum_sum = 0
     for num, count in results.items():
         cum_sum += count   
-    assert cum_sum != expected_output
+    assert cum_sum != not_expected_calls
 
-# # # Call once: total_probability_not_two
-@pytest.mark.parametrize("probs, random_nums, expected_output", [(__probs, __random_nums, 2)])
-def test_positive_next_num_once__total_probability_not_two(probs, random_nums, expected_output):
-    iterations = 1
-    random_gen = RandomGen()
-    random_gen._random_nums = random_nums
-    random_gen._probabilities = probs
-    random_gen._results = {}
+# # Call once: total_probability_not_two
+@pytest.mark.parametrize("iterations, probs, random_nums, not_expected_calls", [(1, __probs, __random_nums, 2)])
+def test_positive_next_num_once__total_probability_not_two(iterations, probs, random_nums, not_expected_calls):
+    random_gen = RandomGenZip(random_nums,probs)
     for _ in range(iterations):
         results = random_gen.next_num()
     for num, count in results.items():
         for numx, prob in zip(random_nums, probs):
             if numx == num:
                 distribution = count/iterations
-    assert distribution != expected_output
+    assert distribution != not_expected_calls
