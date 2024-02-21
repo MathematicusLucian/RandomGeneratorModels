bd6c83ebcb916c2ce4e57b5999fca6c954a2f143
k1n4kut4 <recruitlukejones@gmail.com>
2024-02-21 18:57:13 +0000

    Refined the Performance Test Engine to more dynamic

--- python_model/performance_test.py
+++ python_model/performance_test.py
@@ -5,33 +5,45 @@ from four.argmaxnumpy import RandomGenNumpyArgMax
 from five.binarynumpy import RandomGenBinarySearchNumpy
 from sIx.binarynumpyerrorh import BinaryNumpyWithErrorHandling
 from seven.binarysearch import RandomGenBinarySearch
-from utils.performance_test_engine import generate_test_data, perform_performance_test
+from utils.performance_test_engine import generate_test_data, evaluate_models
 import matplotlib.pyplot as plt
 from utils.plotter import add_data_to_plotter, generate_chart
 
-def test_all_models(random_nums,probabilities,iterations):
-    PYTHON_RANDOM_GEN_CLASSES = [
-        { "class": RandomGen, "name": 'Basic' },
-        { "class": RandomGenZip, "name": 'Zip' },
-        { "class": RandomGenRandomChoices, "name": 'Random Choices' },
-        { "class": RandomGenNumpyArgMax, "name": 'Arg Max' },
-        { "class": BinaryNumpyWithErrorHandling, "name": 'Numpy B Search wv EH' },
-        { "class": RandomGenBinarySearchNumpy, "name": 'Numpy Binary Search' },
-        { "class": RandomGenBinarySearch, "name": 'Binary Search' }
-    ]
-    test_data = generate_test_data(iterations, random_nums, probabilities)
+def test_all_models(models_to_test, random_nums, probabilities, iteration_levels, tolerance):
+    # Pandas dataframe
+    prepared_test_dataframe = generate_test_data(iteration_levels, random_nums, probabilities)
 
-    # Perform performance test
-    return perform_performance_test(test_data, PYTHON_RANDOM_GEN_CLASSES)
+    # Use the Performance Test Engine to run each model
+    return evaluate_models(prepared_test_dataframe, models_to_test, tolerance)
+
+def save_plot_chart(iteration_levels, execution_times_panda):
+    # # Plot execution time vs. input size
+    for generator_name, generator_times in execution_times_panda.items():
+        add_data_to_plotter(plt, iteration_levels, generator_times, generator_name)
+    generate_chart(plt)
 
 if __name__ == '__main__':
     # Test data
     random_nums = [-1, 0, 1, 2, 3]
     probabilities = [0.01, 0.3, 0.58, 0.1, 0.01]
-    iterations = [10, 100, 1000, 10000]  # Add more sizes as needed
-    execution_times_panda = test_all_models(random_nums,probabilities,iterations)
-    # # Plot execution time vs. input size
+    iteration_levels = [10, 100, 1000, 10000]  # Add more levels as required
+    tolerance = 1.0
+    PYTHON_RANDOM_GEN_CLASSES = [
+        { "class": RandomGen, "name": "Basic", "hasConstructor": False },
+        { "class": RandomGenZip, "name": "Zip", "hasConstructor": True },
+        { "class": RandomGenRandomChoices, "name": "Random Choices", "hasConstructor": True },
+        { "class": RandomGenNumpyArgMax, "name": "Arg Max", "hasConstructor": True },
+        { "class": BinaryNumpyWithErrorHandling, "name": "Numpy B Search wv EH", "hasConstructor": True },
+        { "class": RandomGenBinarySearchNumpy, "name": "Numpy Binary Search", "hasConstructor": True },
+        { "class": RandomGenBinarySearch, "name": "Binary Search", "hasConstructor": True }
+    ]
+
+    # Run the performance tests
+    execution_times_panda = test_all_models(PYTHON_RANDOM_GEN_CLASSES, random_nums, probabilities, iteration_levels, tolerance)
+
     for generator_name, generator_times in execution_times_panda.items():
-        print(f"\n{generator_name, iterations, generator_times}")
-        add_data_to_plotter(plt, iterations, generator_times, generator_name)
-    generate_chart(plt)
+        print(f"\n{generator_name, iteration_levels, generator_times}")
+    
+    # This line runs the function to save the chart to file (when running on local machine; disabled on the server)
+    # Could dump this image data in a different format into a database
+    # save_plot_chart(iteration_levels, execution_times_panda)
