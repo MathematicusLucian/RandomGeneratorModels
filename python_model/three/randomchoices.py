fbcbe145be27825b248be2d679bb6f6af8cf6023
k1n4kut4 <recruitlukejones@gmail.com>
2024-02-21 15:59:35 +0000

    Created logic for Python RandomChoices model; tests GREEN - moved to correct directory

---
+++ python_model/three/randomchoices.py
@@ -0,0 +1,30 @@
+import random
+
+class RandomGenRandomChoices(object):
+    # Values that may be returned by next_num()
+    _random_nums = []
+    # Probability of the occurence of random_nums
+    _probabilities = [] 
+    _results = {}
+
+    # Added a constructor
+    def __init__(self, random_nums, probabilities):
+        """
+        Initializes the RandomGen object with random numbers and their corresponding probabilities.
+        """
+        self._random_nums = random_nums
+        self._probabilities = probabilities
+        self._results = {}
+
+    def next_num(self):
+        """
+        Returns one of the randomNums. When this method is called multiple
+        times over a long period, it should return the numbers roughly with
+        the initialized probabilities.
+        """
+        num = random.choices(self._random_nums, weights=self._probabilities)[0]
+        if num in self._results:
+            self._results[num] += 1
+        else:
+            self._results[num] = 1
+        return self._results
